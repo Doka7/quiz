a3e504a863b85068f60c771a0c4d1651788d8e9c
Doka7 <alejandro@127.0.0.1>
2015-05-18 12:55:23 +0200

    Busqueda de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,9 +14,16 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes/question
 exports.index = function(req, res) {
-	models.Quiz.findAll().then(
+	if (req.query.search) {
+		models.Quiz.findAll({where: ["pregunta like ?", '%' + req.query.search + '%']}).then(
  		function(quizes) {res.render('quizes/index', { quizes: quizes});}
- 	).catch(function(error) { next(error);})
+ 		).catch(function(error) { next(error);})
+	}
+	else{
+		models.Quiz.findAll().then(
+ 		function(quizes) {res.render('quizes/index', { quizes: quizes});}
+ 		).catch(function(error) { next(error);})
+	}
 };
 
 // GET /quizes/:id
