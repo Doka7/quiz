97798c4054cee11e598b47c652de9ea4a9378944
Doka7 <alejandro@127.0.0.1>
2015-05-19 11:29:36 +0200

    Login y autorizacion

---
+++ controllers/user_controller.js
@@ -0,0 +1,15 @@
+var users = {
+	admin: {id:1, username:"admin", password:"1234"},
+	doka: {id:2, username:"doka", password:"cr7"}
+};
+
+// Comprueba si el usuario esta registrado en users
+// Si autenticación falla o hay errores se ejecuta callback(error).
+exports.autenticar = function(login, password, callback) {
+if(users[login]){
+if(password === users[login].password){
+callback(null, users[login]);
+}
+else { callback(new Error('Password erróneo.')); }
+} else { callback(new Error('No existe el usuario.'));}
+};
