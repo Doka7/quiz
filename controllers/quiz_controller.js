cec3cecfc6b5a4cbb01b453562da14610dbacaa2
Doka7 <alejandro@127.0.0.1>
2015-05-25 14:17:38 +0200

    Imagenes

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -65,24 +65,27 @@ exports.new = function(req, res) {
 
 //GET /quizes/statistics
 exports.statistics = function(req,res){
-models.Comment.findAll().then(function(comment){
-models.Quiz.findAll().then(function(quizes){
-res.render('quizes/statistics',{quiz: req.quiz, quizes: quizes, comment: comment, errors: []});
-})
-}).catch(function(error) { next(error)});
+	models.Comment.findAll().then(function(comment){
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/statistics',{quiz: req.quiz, quizes: quizes, comment: comment, errors: []});
+		})
+	}).catch(function(error) { next(error)});
 };
 
 
 // POST /quizes/create
 exports.create = function(req, res) {
 	req.body.quiz.UserId = req.session.user.id;
+	if(req.files.image){
+		req.body.quiz.image = req.files.image.name;
+	}
 	var quiz = models.Quiz.build( req.body.quiz );
 	quiz.validate().then(function(err){
 		if (err) {
 			res.render('quizes/new', {quiz: quiz, errors: err.errors});
 		} else {
 			quiz // save: guarda en DB campos pregunta y respuesta de quiz
-			.save({fields: ["pregunta", "respuesta", "UserId"]})
+			.save({fields: ["pregunta", "respuesta", "UserId", "image"]})
 			.then( function(){ res.redirect('/quizes')})
 		} // res.redirect: Redirección HTTP a lista de preguntas
 	}).catch(function(error){next(error)});
@@ -97,18 +100,21 @@ exports.edit = function(req, res) {
 
 // PUT /quizes/:id
 exports.update = function(req, res) {
-req.quiz.pregunta = req.body.quiz.pregunta;
-req.quiz.respuesta = req.body.quiz.respuesta;
+	if(req.files.image){
+		req.quiz.image = req.files.image.name;
+	}
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
 
-req.quiz.validate().then(function(err){
-	if (err) {
-		res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
-	} else {
-		req.quiz // save: guarda campos pregunta y respuesta en DB
-		.save( {fields: ["pregunta", "respuesta"]})
-		.then( function(){ res.redirect('/quizes');});
-	} // Redirección HTTP a lista de preguntas (URL relativo)
-}).catch(function(error){next(error)});
+	req.quiz.validate().then(function(err){
+		if (err) {
+			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+		} else {
+			req.quiz // save: guarda campos pregunta y respuesta en DB
+			.save( {fields: ["pregunta", "respuesta", "image"]})
+			.then( function(){ res.redirect('/quizes');});
+		} // Redirección HTTP a lista de preguntas (URL relativo)
+	}).catch(function(error){next(error)});
 };
 
 // DELETE /quizes/:id
--- models/quiz.js
+++ models/quiz.js
@@ -10,6 +10,9 @@ module.exports = function(sequelize, DataTypes) {
 			respuesta: {
 				type: DataTypes.STRING,
 				validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+			},
+			image: {
+				type: DataTypes.STRING
 			}
 		});
 } 
