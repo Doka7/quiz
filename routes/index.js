95a3941802b8b2661fd7c60af45908e97feaa29c
Doka7 <alejandro@127.0.0.1>
2015-05-25 11:27:15 +0200

    Registro de usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var userController = require('../controllers/user_controller');
 
 // Pagina de entrada (home page)
 router.get('/', function(req, res) {
@@ -15,15 +16,23 @@ router.get('/credit', function(req, res) {
 }); 
 
 router.get('/statistics', quizController.statistics);
-// Autoload de comandos con :quizId
+// Autoload de comandos con ids
 router.param('quizId', quizController.load); // autoload :quizId
 router.param('commentId', commentController.load); // autoload :commentId
+router.param('userId', userController.load); // autoload :userId
 // Definición de rutas de sesion
 router.get('/login', sessionController.new); // formulario login
 router.post('/login', sessionController.create); // crear sesión
 router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
 sessionController.loginRequired, commentController.publish);
 router.get('/logout', sessionController.destroy); // destruir sesión
+
+// Definición de rutas de cuenta
+router.get('/user', userController.new); // formulario sign un
+router.post('/user', userController.create); // registrar usuario
+router.get('/user/:userId(\\d+)/edit', sessionController.loginRequired, userController.edit); // editar información de cuenta
+router.put('/user/:userId(\\d+)', sessionController.loginRequired, userController.update); // actualizar información de cuenta
+router.delete('/user/:userId(\\d+)', sessionController.loginRequired, userController.destroy); // borrar cuenta
 
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
